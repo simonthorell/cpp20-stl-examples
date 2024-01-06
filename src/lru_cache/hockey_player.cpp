92118f5c2701bbb18478167436a3750f5744a302
Simon Thorell <kontakt@simonthorell.se>
2024-01-06 20:20:34 +0100

    Hockey player comments

--- src/lru_cache/hockey_player.cpp
+++ src/lru_cache/hockey_player.cpp
@@ -1,7 +1,12 @@
 #include "lru_cache/hockey_player.h"
 
 //==============================================================================
-// Constructor
+// Constructor: HockeyPlayer
+// Parameters: id - id of player
+//             name - name of player
+//             jersey - jersey number of player
+//             teamName - name of team player is on
+// Description: Constructor for HockeyPlayer.
 //==============================================================================
 HockeyPlayer::HockeyPlayer(int id, const std::string& name, int jersey, const std::string& teamName)
     : id(id), name(name), jersey(jersey), teamName(teamName) {}
