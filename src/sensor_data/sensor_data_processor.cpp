6de9a39c3fd9a504aa9d647f4b6fedccef3b20ca
Simon Thorell <kontakt@simonthorell.se>
2024-01-10 19:36:44 +0100

    Improved readability

--- src/sensor_data/sensor_data_processor.cpp
+++ src/sensor_data/sensor_data_processor.cpp
@@ -11,7 +11,7 @@ SensorDataProcessor::SensorDataProcessor(const std::vector<SensorData>& data)
     : sensorData(data) {}
 //=============================================================================
 // Function: countAltitudeData
-// Description: Counts and prints the number of altitude data entries from a 
+// Description: Counts and returns the number of altitude data entries from a 
 //              specific date given by year, month, and day parameters.
 //=============================================================================
 int SensorDataProcessor::countAltitudeData(uint16_t year, uint8_t month, uint8_t day) {
@@ -19,13 +19,15 @@ int SensorDataProcessor::countAltitudeData(uint16_t year, uint8_t month, uint8_t
     std::time_t targetDay = std::mktime(&timeStruct);
     timeStruct.tm_mday += 1; // Increment day by 1 to get the next day
     std::time_t nextDay = std::mktime(&timeStruct);
+
     // Filter to only Altitude data entries in the given time range
     auto isAltitudeInTimeRange = [targetDay, nextDay] (SensorData& data) {
         std::time_t dataTime = data.getTime();
         return data.getSensorType() == SensorType::Altitude && 
                dataTime >= targetDay && dataTime < nextDay;
     };
-    // Count & return the number of altitude data entries on the given day
+
+    // Count & return the number of altitude data entries on the target day
     return std::ranges::count_if(sensorData, isAltitudeInTimeRange);
 }
 //=============================================================================
@@ -39,24 +41,32 @@ bool SensorDataProcessor::checkMaxSpeed(float maxSpeed) {
     auto speedReached = std::views::filter(sensorData, [maxSpeed](SensorData& data) {
         return data.getSensorType() == SensorType::SpeedInKmh && data.getValue() > maxSpeed;
     });
+
     // Return true once/if any entries exceed the max speed
     return speedReached.begin() != speedReached.end();
 }
 //=============================================================================
 // Function: updateFuelConsumption
-// Description: Updates all FuelConsumption data entries by the given factor.
+// Description: Updates all FuelConsumption data entries by the given factor
+//              in a single iteration. It only iterates if there's applicable
+//              data entries and the factor is not 0 (no need to transform).
+//              Returns true if transformation was applied, otherwise false.
 //=============================================================================
 bool SensorDataProcessor::updateFuelConsumption(float factor) {
-    // Create a combined view for filtering and transforming
-    auto updateView = sensorData | std::views::filter([](SensorData& data) {
-        return data.getSensorType() == SensorType::FuelConsumption;
-    }) | std::views::transform([factor](SensorData& data) -> SensorData& {
-        data.setValue(data.getValue() * factor);
-        return data; // The return value isn't used since we're modifying in place
-    });
-    // Check if factor is non-zero and updateView is not empty (transformation applied).
+    // Create a combined view for filtering and transforming sensor data
+    auto updateView = sensorData 
+        | std::views::filter([](SensorData& data) {
+            return data.getSensorType() == SensorType::FuelConsumption;
+        })
+        | std::views::transform([factor](SensorData& data) -> SensorData& {
+            data.setValue(data.getValue() * factor);
+            return data; // Modifying value in place, return value not used
+        });
+
+    // Check if factor is not 0 and if any data in range to transform
     if (factor != 0 && !std::ranges::empty(updateView)) {
-        return true; // Transformation was applied during iteration over updateView
+        return true; // Transformation was applied during iteration of view
     }
+
     return false; // Transformation was not applied
 }
