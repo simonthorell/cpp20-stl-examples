bd17fb31a0540dd783fda1311cb234c2999cb4d7
Simon Thorell <kontakt@simonthorell.se>
2024-01-11 01:21:17 +0100

    Add unit tests for LRU template

---
+++ tests/test_lru_cache.cpp
@@ -0,0 +1,57 @@
+#include "lru_cache/lru_cache.h"
+#include "gtest/gtest.h"
+
+class LRUCacheTest : public ::testing::Test {
+protected:
+    LRUCache<HockeyPlayer>* cache;
+
+    void SetUp() override {
+        cache = new LRUCache<HockeyPlayer>(3); // Cache with capacity 3
+    }
+
+    void TearDown() override {
+        delete cache;
+    }
+};
+
+TEST_F(LRUCacheTest, IsEmptyInitially) {
+    EXPECT_TRUE(cache->isEmpty());
+}
+
+TEST_F(LRUCacheTest, AddsAndRetrievesSingleItem) {
+    HockeyPlayer* player = new HockeyPlayer(/* parameters */);
+    cache->refer(1, player);
+    EXPECT_EQ(cache->getPlayer(1), player);
+}
+
+TEST_F(LRUCacheTest, EvictsLRUItem) {
+    HockeyPlayer* player1 = new HockeyPlayer(/* parameters */);
+    HockeyPlayer* player2 = new HockeyPlayer(/* parameters */);
+    HockeyPlayer* player3 = new HockeyPlayer(/* parameters */);
+    HockeyPlayer* player4 = new HockeyPlayer(/* parameters */);
+
+    cache->refer(1, player1);
+    cache->refer(2, player2);
+    cache->refer(3, player3);
+    // Cache is full now
+    cache->refer(4, player4);
+    // Player 1 should be evicted
+    EXPECT_EQ(cache->getPlayer(1), nullptr);
+    EXPECT_EQ(cache->getPlayer(4), player4);
+}
+
+TEST_F(LRUCacheTest, UpdatesLRUOrder) {
+    HockeyPlayer* player1 = new HockeyPlayer(/* parameters */);
+    HockeyPlayer* player2 = new HockeyPlayer(/* parameters */);
+    HockeyPlayer* player3 = new HockeyPlayer(/* parameters */);
+
+    cache->refer(1, player1);
+    cache->refer(2, player2);
+    cache->refer(3, player3);
+    // Accessing player 1 again
+    cache->getPlayer(1);
+    cache->refer(4, new HockeyPlayer(/* parameters */)); // This should evict player 2
+
+    EXPECT_EQ(cache->getPlayer(1), player1);
+    EXPECT_EQ(cache->getPlayer(2), nullptr);
+}
--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -5,6 +5,7 @@
 // Include Test Suites
 #include "test_sensor_data_processor.cpp"
 #include "test_atm_simulator.cpp"
+#include "test_lru_cache.cpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
